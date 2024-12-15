0913b3f76d1928f44fe1b81a87a272e052fe5c99
Olexiy Nikolayev <alexpronick@gmail.com>
2024-12-15 17:23:05 +0100

    Clean up start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,6 @@ echo 'starting'
 
 printenv GOOGLE_JSON > /opt/google.json
 
-#sed -z 's/\n/\\n/g; s/..$//' /opt/google.json > /opt/google.json
 chmod 644 /opt/google.json
-echo "\n\n"
-cat /opt/google.json
-echo "\n\n"
 
 python3 server.py srv --port 8080 --debug
